145ee4fd3963bf0277594d381333243421f82830
lushg <lushg-tcxg@qq.com>
2020-04-04 18:48:59 +0800

    tree

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,70 @@
 <template>
   <div id="app">
-    <tree></tree>
+    <tree :treeData="treeData" ref="tree">
+      <template v-slot="{ data, showNext }">
+        <div class="node-render">
+          <div class="icon-container">
+            <span
+              :class="{
+                icon: true,
+                'show-next': !showNext,
+                'no-next': !data.children || !data.children.length
+              }"
+              @click.stop="showChildren(data.id, !showNext)">
+            </span>
+          </div>
+          <div class="title">{{ data.title }}</div>
+        </div>
+      </template>
+    </tree>
   </div>
 </template>
 
 <script>
 import Tree from '@/components/super-tree/tree'
+function generateTree () {
+  console.time('ge')
+  let map = new Map()
+  let result = []
+  for (let i = 0; i < 1000; i++) {
+    let list = new Array(Math.floor(Math.random() * 40)).fill('hello')
+    let data = {
+      title: `节点${i}${list.join('')}`,
+      id: i,
+      parentId: undefined,
+      children: []
+    }
+    map.set(i, data)
+    if (result.length > 90000) {
+      let length = map.size - 1
+      data.parentId = Math.floor(Math.random() * length)
+      let parent = map.get(data.parentId)
+      parent.children.push(data)
+    } else {
+      result.push(data)
+    }
+  }
+  console.timeEnd('ge')
+  return result
+}
 
 export default {
   name: 'App',
   components: {
     Tree
+  },
+  data () {
+    return {
+      treeData: []
+    }
+  },
+  created () {
+    this.treeData = generateTree()
+  },
+  methods: {
+    showChildren (id, show) {
+      this.$refs.tree.showChildren(id, show)
+    }
   }
 }
 </script>
@@ -24,4 +78,30 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+html {
+  /* background: #333; */
+}
+</style>
+
+<style lang="stylus" scoped>
+.node-render
+  display flex
+  .icon-container
+    flex-shrink 0
+    display flex
+    align-items center
+    padding 0 10px
+    .icon
+      border 8px solid transparent
+      cursor pointer
+      border-top-color #ffa741
+      border-bottom-width 0
+      transition transform .1s ease-out
+      &.show-next
+        transform rotate(-90deg)
+
+      &.no-next
+        border 4px solid #ffa741
+        border-radius 50%
+
 </style>
--- src/components/super-tree/tree.vue
+++ src/components/super-tree/tree.vue
@@ -9,73 +9,87 @@
         paddingLeft: `${levelData[node[props.key]] * 20}px`
       }"
       class="node">
-      <span v-if="foldData[node.id]">展开</span>{{ node.title }}
+      <node-content
+        :data="node"
+        :level="levelData[node[props.key]]"
+        :showNext="!foldData[node.id]">
+      </node-content>
     </div>
   </div>
 </template>
 
 <script>
-function generateTree () {
-  console.time('ge')
-  let map = {}
-  let result = []
-  for (let i = 0; i < 100000; i++) {
-    let list = new Array(Math.floor(Math.random() * 40)).fill('hello')
-    let data = {
-      title: `节点${i}${list.join('')}`,
-      id: i,
-      parentId: undefined,
-      children: []
-    }
-    map[i] = data
-    if (result.length > 300000) {
-      let length = Object.keys(map).length - 1
-      data.parentId = Math.floor(Math.random() * length)
-      let parent = map[data.parentId]
-      parent.children.push(data)
-    } else {
-      result.push(data)
-    }
-  }
-  console.timeEnd('ge')
-  return result
-}
 
-export default {
+export default { 
+  components: {
+    'node-content': {
+      props: ['data', 'level', 'showNext'],
+      computed: {
+        slotData () {
+          return {
+            data: this.data,
+            level: this.level,
+            showNext: this.showNext
+          }
+        }
+      },
+      render (h) {
+        let slot = this.$parent.$scopedSlots.default
+        let props = this.$parent.props
+        return slot ? slot(this.slotData) : (<div>{ this.data[props.label] }</div>)
+      }
+    },
+  },
   props: {
     treeData: {
-      default: () => generateTree()
+      required: true,
     },
-    props: {
-      default: () => ({
-        key: 'id',
-        childrenKey: 'children'
-      })
+    customProps: {
+      default: () => ({})
     },
+    // 同时渲染的节点数量
     renderNum: {
       default: 100
     },
+    // 缓冲区大小，用于节流，避免滚动条上下滚动很短距离就立刻实时计算动态列表
     buffer: {
       default: 25
     }
   },
   data () {
     return {
-      foldData: {},
-      levelData: {},
-      start: 0,
-      preStart: 0,
+      foldData: {}, // 被折叠子项 key => true
+      levelData: {}, // 节点层级 key => level
+      start: 0, // 动态渲染开始下标，preRenderList[start]对应渲染的第一个DOM
+      scrollPosition: -1, // 记录滚动条位置
     }
   },
   computed: {
+    props () {
+      let defaultProps = {
+        key: 'id',
+        childrenKey: 'children',
+        label: 'title'
+      }
+      return Object.assign({}, defaultProps, this.customProps)
+    },
     treeList () {
-      console.time('treeToList')
-      let res = this.treeToList(this.treeData)
-      console.timeEnd('treeToList')
-      return res
+      return this.treeToList(this.treeData)
+    },
+    preRenderList () {
+      // treeList过滤掉被折叠的子节点
+      let list = [], foldLevel = Infinity
+      this.treeList.forEach(data => {
+        let key = data[this.props.key]
+        if (this.levelData[key] <= foldLevel) {
+          list.push(data)
+          foldLevel = this.foldData[key] ? this.levelData[key] : Infinity
+        }
+      })
+      return list
     },
     renderList () {
-      return this.treeList.slice(this.start, this.start + this.renderNum)
+      return this.preRenderList.slice(this.start, this.start + this.renderNum)
     }
   },
   created () {
@@ -89,47 +103,69 @@ export default {
       })
       return result
     },
-
+    scroll () {
+      this.checkRender()
+    },
     checkRender () {
+      if (this.scrollPosition >= 0) return
+      let len = this.$refs.node.length
+      if (!len) return
+      this.scrollPosition = this.$el.scrollTop
+      this.freezeScrollBar()
+
+      let rect = this.$el.getBoundingClientRect()
+      let y = (rect.top + rect.bottom) / 2
+      // 在容器最中间的元素
+      let centerNode = this.$refs.node.find(n => {
+        let r = n.getBoundingClientRect()
+        return r.top <= y && r.bottom >= y
+      })
+
+      centerNode = centerNode || this.$refs.node[len - 1]
+      let identify = centerNode.getAttribute('identify')
+      let targetTop = centerNode.getBoundingClientRect().top
+      let sourceIdx = this.preRenderList.findIndex(data => String(data[this.props.key]) === identify)
+      let preStart = 0
+      if (sourceIdx !== -1) {
+        preStart = sourceIdx - Math.floor(this.renderNum / 2)
+      }
+      if (Math.abs(preStart - this.start) < this.buffer) {
+        this.unfreezeScrollBar()
+        return
+      }
+      this.start = Math.max(0, preStart)
+
+      // 还原滚动条位置
       this.$nextTick(() => {
-        if (!this.$refs.node.length) return
-        let rect = this.$el.getBoundingClientRect()
-        let y = (rect.top + rect.bottom) / 2
-        // 在容器最中间的元素
-        let scrollTop = this.$el.scrollTop
-        let centerNode = this.$refs.node.find(n => {
-          let r = n.getBoundingClientRect()
-          return r.top <= y && r.bottom >= y
-        })
-        if (!centerNode) {
-          centerNode = this.$refs.node[this.$refs.node.length - 1]
+        let centerDom = this.$refs.node.find(dom => dom.getAttribute('identify') === identify)
+        if (!centerDom) {
+          this.unfreezeScrollBar()
+          this.checkRender()
+          return
         }
-        let identify = centerNode.getAttribute('identify')
-        let targetTop = centerNode.getBoundingClientRect().top
-        let sourceIdx = this.treeList.findIndex(data => String(data[this.props.key]) === identify)
-        let oldStart = this.start
-        let preStart = 0
-        if (sourceIdx !== -1) {
-          preStart = sourceIdx - Math.floor(this.renderNum / 2)
-        }
-        if (Math.abs(preStart - this.start) >= this.buffer) {
-          this.start = Math.max(0, preStart)
-        }
-        if (oldStart === this.start) return
-        // 还原滚动条位置
-        this.$nextTick(() => {
-          let centerDom = this.$refs.node.find(dom => dom.getAttribute('identify') === identify)
-          if (!centerDom) {
-            window.requestAnimationFrame(this.checkRender)
-            return
-          }
-          let deltaScroll = centerDom.getBoundingClientRect().top - targetTop
-          this.$el.scrollTop = scrollTop + deltaScroll
-        })
+        let deltaScroll = centerDom.getBoundingClientRect().top - targetTop
+        this.$el.scrollTop += deltaScroll
+        this.unfreezeScrollBar()
       })
     },
-    scroll () {
-      window.requestAnimationFrame(this.checkRender)
+
+    freezeScrollBar () {
+      window.requestAnimationFrame(() => {
+        if (this.scrollPosition >= 0) {
+          this.$el.scrollTop = this.scrollPosition
+          this.freezeScrollBar()
+        }
+      })
+    },
+    unfreezeScrollBar () {
+      this.scrollPosition = -1
+    },
+
+    /**
+     * 对外接口
+     */
+    showChildren (key, unfold) {
+      this.$set(this.foldData, key, !unfold)
     }
   }
 }
